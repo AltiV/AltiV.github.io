bc6095f78be469677f63ff04fb4347515fdaf6eb
AltiV <vuong.alan.t@gmail.com>
2020-10-20 21:31:55 -0400

    Updates

---
+++ precache-manifest.20fd9db29f953e410ea81be288a83732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ad08234d6ad2703d343aa0c79ed389",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8eed12a71b1a844816e4",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32315aab3d7c6cb0cc56",
+    "url": "/portfolio/static/js/2.b441d618.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/portfolio/static/js/2.b441d618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eed12a71b1a844816e4",
+    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
