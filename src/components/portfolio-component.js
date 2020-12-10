1680857168c2be2edd55aa1f9a71a1ee6747fac5
AltiV <vuong.alan.t@gmail.com>
2020-12-10 00:35:33 -0500

    Updated resume

--- src/components/portfolio-component.js
+++ src/components/portfolio-component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 import {
   List,
@@ -10,66 +10,80 @@ import {
   Avatar,
   IconButton,
   Typography,
-  Chip,
-} from "@material-ui/core";
+  Chip
+} from '@material-ui/core'
 
-import WebIcon from "@material-ui/icons/Web";
-import GitHubIcon from "@material-ui/icons/GitHub";
+import WebIcon from '@material-ui/icons/Web'
+import GitHubIcon from '@material-ui/icons/GitHub'
 
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
 
 // CSS STYLES
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.primary.light,
+    backgroundColor: theme.palette.primary.light
   },
   button: {
-    color: "white",
+    color: 'white'
   },
   websiteLink: {
-    color: "white",
-    textDecoration: "none",
+    color: 'white',
+    textDecoration: 'none'
   },
   technologyChip: {
-    margin: "0 0.1rem",
-  },
-}));
+    margin: '0 0.1rem'
+  }
+}))
 
 const projects = [
   {
-    name: "Issue Tracker",
-    description: "Track user-submitted projects and corresponding issues.",
-    technologies: ["MERN Stack", "Bootstrap"],
-    website: "https://issue-tracker-mern.herokuapp.com/",
-    github: "https://github.com/AltiV/Issue-Tracker",
+    name: 'ClueWords',
+    description:
+      'A web application version of the CodeNames board game, complete with authentication.',
+    technologies: [
+      'MERN Stack',
+      'Material UI',
+      'Web Sockets',
+      'multer-s3',
+      'Stripe'
+    ],
+    website: 'https://afternoon-woodland-86224.herokuapp.com/',
+    github: 'https://github.com/hatchways/team-fruit-loops'
   },
   {
-    name: "KartRider Rush+ Unofficial Manual",
+    name: 'Issue Tracker',
+    description: 'Track user-submitted projects and corresponding issues.',
+    technologies: ['MERN Stack', 'Bootstrap'],
+    website: 'https://issue-tracker-mern.herokuapp.com/',
+    github: 'https://github.com/AltiV/Issue-Tracker'
+  },
+  {
+    name: 'KartRider Rush+ Unofficial Manual',
     description:
       "An information repository for the KartRider Rush+ mobile application, holding data for the game's karts, characters, tracks, and more. Includes a basic quiz component.",
-    technologies: ["Angular", "Material UI"],
-    website: "https://krrplus.web.app/",
-    github: null,
+    technologies: ['Angular', 'Material UI'],
+    website: 'https://krrplus.web.app/',
+    github: null
   },
   {
-    name: "Personal Website (Old)",
-    description: "My first portfolio website, using basic web techologies.",
-    technologies: ["HTML", "Javascript"],
-    website: "https://altiv.github.io/website/",
-    github: "https://github.com/AltiV/website",
-  },
-];
+    name: 'Personal Website (Old)',
+    description: 'My first portfolio website, using basic web techologies.',
+    technologies: ['HTML', 'Javascript'],
+    website: 'https://altiv.github.io/website/',
+    github: 'https://github.com/AltiV/website'
+  }
+]
 
 const Portfoilo = () => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <List className={classes.root}>
       {projects.map((project, key) => (
         <>
-          <ListItem alignItems="flex-start" key={key}>
+          <ListItem alignItems='flex-start' key={key}>
             <ListItemAvatar>
-              <Avatar alt={project.name} src="">
+              <Avatar alt={project.name} src=''>
                 {project.name[0]}
               </Avatar>
             </ListItemAvatar>
@@ -78,8 +92,8 @@ const Portfoilo = () => {
                 <>
                   <a
                     href={project.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    target='_blank'
+                    rel='noopener noreferrer'
                     className={classes.websiteLink}
                   >
                     {project.name}
@@ -97,33 +111,33 @@ const Portfoilo = () => {
               secondary={
                 <React.Fragment>
                   <Typography
-                    component="span"
-                    variant="body2"
-                    color="textPrimary"
+                    component='span'
+                    variant='body2'
+                    color='textPrimary'
                   >
                     {project.description}
                   </Typography>
                 </React.Fragment>
               }
             />
-            <ListItemSecondaryAction style={{ color: "white" }}>
-              <IconButton edge="end">
+            <ListItemSecondaryAction style={{ color: 'white' }}>
+              <IconButton edge='end'>
                 <a
                   href={project.website}
                   className={classes.button}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   <WebIcon />
                 </a>
               </IconButton>
               {project.github && (
-                <IconButton edge="end">
+                <IconButton edge='end'>
                   <a
                     href={project.github}
                     className={classes.button}
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
                     <GitHubIcon />
                   </a>
@@ -131,11 +145,11 @@ const Portfoilo = () => {
               )}
             </ListItemSecondaryAction>
           </ListItem>
-          <Divider variant="inset" component="li" />
+          <Divider variant='inset' component='li' />
         </>
       ))}
     </List>
-  );
-};
+  )
+}
 
-export default Portfoilo;
+export default Portfoilo
